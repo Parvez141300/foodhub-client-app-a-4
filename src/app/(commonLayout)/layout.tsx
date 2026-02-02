7b584162953416ff9d81602586f6617c5035b718
Parvez141300 <parvezhossain744471@gmail.com>
2026-02-02 19:42:32 +0600

    created nav bar and also theme provider

---
+++ src/app/(commonLayout)/layout.tsx
@@ -0,0 +1,13 @@
+import { Navbar1 } from "@/components/layouts/navbar1";
+import React from "react";
+
+const CommonLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div>
+      <Navbar1 />
+      <div className="max-w-7xl mx-auto my-6">{children}</div>
+    </div>
+  );
+};
+
+export default CommonLayout;
