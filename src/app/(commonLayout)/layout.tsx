2d50045bd010f95414a6eaa74fe271a4630d58a2
Parvez141300 <parvezhossain744471@gmail.com>
2026-02-19 16:34:37 +0600

    created a footer section

--- src/app/(commonLayout)/layout.tsx
+++ src/app/(commonLayout)/layout.tsx
@@ -1,3 +1,4 @@
+import { EcommerceFooter1 } from '@/components/layouts/common/ecommerce-footer1';
 import { Navbar1 } from '@/components/layouts/common/navbar1';
 import React from 'react';
 
@@ -5,7 +6,8 @@ const CommonLayout = ({children} : {children: React.ReactNode}) => {
     return (
         <div>
             <Navbar1 />
-            <div className="max-w-7xl mx-auto my-6 px-4">{children}</div>
+            <div className="max-w-7xl mx-auto my-6 px-4 min-h-screen">{children}</div>
+            <EcommerceFooter1 />
         </div>
     );
 };
