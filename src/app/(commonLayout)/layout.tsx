61e3cfa02609176cf2878254407d57da2c035eb4
Parvez141300 <parvezhossain744471@gmail.com>
2026-02-20 22:03:41 +0600

    edited the home page

--- src/app/(commonLayout)/layout.tsx
+++ src/app/(commonLayout)/layout.tsx
@@ -1,7 +1,5 @@
 import { EcommerceFooter1 } from "@/components/layouts/common/ecommerce-footer1";
-import { HomeCarousel } from "@/components/layouts/common/home-carousel";
 import { Navbar1 } from "@/components/layouts/common/navbar1";
-import NewsletterPromo from "@/components/layouts/common/NewsLetterPromo";
 import { userServices } from "@/services/user.service";
 import React from "react";
 
@@ -11,9 +9,7 @@ const CommonLayout = async ({ children }: { children: React.ReactNode }) => {
     <div>
       <Navbar1 session={session} />
       <div className="max-w-7xl mx-auto my-6 px-4 min-h-screen space-y-8">
-        <HomeCarousel />
         {children}
-        <NewsletterPromo />
       </div>
       <EcommerceFooter1 />
     </div>
--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -1,3 +1,12 @@
+import { HomeCarousel } from "@/components/layouts/common/home-carousel";
+import NewsletterPromo from "@/components/layouts/common/NewsLetterPromo";
+
 export default function HomePage() {
-  return <div>this is home page</div>;
+  return (
+    <div className="space-y-8">
+      <HomeCarousel />
+      
+      <NewsletterPromo />
+    </div>
+  );
 }
