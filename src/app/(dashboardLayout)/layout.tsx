2f0e4e50358d243da91e80af67ec6b8075bf9d56
Parvez141300 <parvezhossain744471@gmail.com>
2026-02-23 15:52:53 +0600

    added a dashboard top menu bar in dashboard

--- src/app/(dashboardLayout)/layout.tsx
+++ src/app/(dashboardLayout)/layout.tsx
@@ -1,4 +1,5 @@
 import { AppSidebar } from "@/components/layouts/dashboard/app-sidebar";
+import DashboardTopMenuBar from "@/components/layouts/dashboard/dashboard-top-menu-bar";
 import {
   Breadcrumb,
   BreadcrumbItem,
@@ -31,23 +32,13 @@ export default async function DashboardLayout({
     <SidebarProvider>
       <AppSidebar userInfo={userInfo} />
       <SidebarInset>
-        <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
+        <header className="flex h-16 shrink-0 items-center border-b px-4">
           <SidebarTrigger className="-ml-1" />
           <Separator
             orientation="vertical"
             className="mr-2 data-[orientation=vertical]:h-4"
           />
-          <Breadcrumb>
-            <BreadcrumbList>
-              <BreadcrumbItem className="hidden md:block">
-                <BreadcrumbLink href="#">Build Your Application</BreadcrumbLink>
-              </BreadcrumbItem>
-              <BreadcrumbSeparator className="hidden md:block" />
-              <BreadcrumbItem>
-                <BreadcrumbPage>Data Fetching</BreadcrumbPage>
-              </BreadcrumbItem>
-            </BreadcrumbList>
-          </Breadcrumb>
+          <DashboardTopMenuBar userInfo={userInfo} />
         </header>
         <div className="flex flex-1 flex-col gap-4 p-4">
           {userInfo?.role === Roles.ADMIN
---
+++ src/components/layouts/dashboard/dashboard-top-menu-bar.tsx
@@ -0,0 +1,39 @@
+import {
+  Avatar,
+  AvatarBadge,
+  AvatarFallback,
+  AvatarImage,
+} from "@/components/ui/avatar";
+import React from "react";
+import { ModeToggle } from "../common/ModeToggle";
+
+interface UserInfo {
+  name: string;
+  role: string;
+}
+
+const DashboardTopMenuBar = ({ userInfo }: { userInfo: UserInfo }) => {
+  console.log(userInfo);
+  return (
+    <div className="flex justify-between items-center w-full">
+      <h3 className="text-xl font-bold">Dashboard</h3>
+      <div className="flex items-center gap-2">
+        <ModeToggle />
+        <div className="flex items-center gap-3">
+          <Avatar size="lg">
+            <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+            <AvatarFallback>CN</AvatarFallback>
+            <AvatarBadge className="bg-green-600 dark:bg-green-800" />
+          </Avatar>
+
+          <div className="flex flex-col">
+            <h4 className="text-sm font-semibold">{userInfo?.name}</h4>
+            <h5 className="tex-xs">Role: {userInfo?.role}</h5>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DashboardTopMenuBar;
