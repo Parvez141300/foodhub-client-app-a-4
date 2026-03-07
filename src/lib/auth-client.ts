12bfdfa9a32b29019e1aeeb9ccec92400fdda948
Parvez141300 <parvezhossain744471@gmail.com>
2026-03-07 11:42:03 +0600

    edited the auth-client.ts file to give a env name base url

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,5 +1,9 @@
+import { env } from "@/env"
 import { createAuthClient } from "better-auth/react"
+
+const NEXT_PUBLIC_BACKEND_URL = env.NEXT_PUBLIC_BACKEND_URL
+
 export const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: "http://localhost:5000"
+    baseURL: `${NEXT_PUBLIC_BACKEND_URL}`
 })
